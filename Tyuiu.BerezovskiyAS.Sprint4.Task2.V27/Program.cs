73f6962859ee8b35c4b03283c53b58bfcbb74a94
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-11-12 01:43:50 +0500

    Task2

---
+++ Tyuiu.BerezovskiyAS.Sprint4.Task2.V27/Program.cs
@@ -0,0 +1,32 @@
+﻿using Tyuiu.BerezovskiyAS.Sprint4.Task2.V27.Lib;
+{
+    DataService ds = new DataService();
+    Random rnd = new Random();
+    Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                      *");
+    Console.WriteLine("*************************************************************************");
+    int len;
+    Console.WriteLine("Введите количество элементов массива:");
+    len = Convert.ToInt32(Console.ReadLine());
+
+    int[] numsArray = new int[len];
+    for (int i = 0; i <= len - 1; i++)
+    {
+        numsArray[i] = rnd.Next(2, 7);
+
+    }
+    Console.WriteLine();
+    Console.WriteLine("Массив: ");
+    for (int i = 0; i <= len - 1; i++)
+    {
+        Console.Write(numsArray[i] + "\t");
+    }
+    Console.WriteLine();
+    Console.WriteLine();
+    Console.WriteLine("*************************************************************************");
+    Console.WriteLine("* РЕЗУЛЬТАТ:                                                            *");
+    Console.WriteLine("*************************************************************************");
+    int res = ds.Calculate(numsArray);
+    Console.WriteLine(res);
+
+    Console.ReadKey();
+}
