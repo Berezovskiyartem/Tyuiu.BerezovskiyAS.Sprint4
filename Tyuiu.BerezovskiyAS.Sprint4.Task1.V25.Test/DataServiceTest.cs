7254b0b7a030636d4b84a1d56553874e5348d1ab
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-11-12 01:39:54 +0500

    Task1

---
+++ Tyuiu.BerezovskiyAS.Sprint4.Task1.V25.Test/DataServiceTest.cs
@@ -0,0 +1,18 @@
+﻿using Tyuiu.BerezovskiyAS.Sprint4.Task1.V25.Lib;
+namespace Tyuiu.BerezovskiyAS.Sprint4.Task1.V25.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+            DataService ds = new DataService();
+
+            int[] array = { 6, 3, 6, 4, 7, 4, 6, 5, 2, 3, 6, 3, 6, 2 };
+            int res = ds.Calculate(array);
+            int wait = 42;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
