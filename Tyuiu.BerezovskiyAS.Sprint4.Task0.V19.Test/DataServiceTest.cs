85d938e9e00f44d0ad19738ab35319bd2c2a2ccf
Артём Березовский <Tyuiu.BerezovskiyAS@outlook.com>
2025-11-12 01:35:36 +0500

    Add project files.

---
+++ Tyuiu.BerezovskiyAS.Sprint4.Task0.V19.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.BerezovskiyAS.Sprint4.Task0.V19.Lib;
+namespace Tyuiu.BerezovskiyAS.Sprint4.Task0.V19.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidGetMultOddArrEl()
+        {
+            DataService ds = new DataService();
+            int[] array = { 9, 5, 7, 4, 5, 3, 7, 8, 9, 1 };
+            int res = ds.GetMultOddArrEl(array);
+            int wait = 297675;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.BerezovskiyAS.Sprint4.Task0.V19/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.BerezovskiyAS.Sprint4.Task0.V19.Lib;
+{
+    DataService ds = new DataService();
+    Console.Title = "Спринт #4 | Выполнил: Березовский А. С. | ПИНб-25-1";
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* Спринт #4                                                               *");
+    Console.WriteLine("* Задание #0                                                              *");
+    Console.WriteLine("* Вариант #19                                                             *");
+    Console.WriteLine("* Выполнил: Березовский Артём Станиславович | ПИНб-25-1                   *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* УСЛОВИЕ:                                                                *");
+    Console.WriteLine("* Дан одномерный целочисленный массив на 10 элементов, заполненный        *");
+    Console.WriteLine("* статическими значениями в диапазоне от 0 до 9. Написать программу,      *");
+    Console.WriteLine("* которая считает произведение нечетных элементов массива.                *");
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+    Console.WriteLine("***************************************************************************");
+
+    int[] array = { 9, 5, 7, 4, 5, 3, 7, 8, 9, 1 };
+
+    Console.WriteLine("Массив: ");
+    for (int i = 0; i <= array.Length - 1; i++)
+    {
+        Console.Write(array[i] + "\t");
+    }
+    Console.WriteLine();
+
+    Console.WriteLine("***************************************************************************");
+    Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+    Console.WriteLine("***************************************************************************");
+
+    Console.WriteLine("Произведение нечётных элементов массива = " + ds.GetMultOddArrEl(array));
+    Console.ReadKey();
+}
